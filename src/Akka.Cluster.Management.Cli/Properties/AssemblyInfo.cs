1880aa6b8a09aaf066a5c2145ba71b9c2801c3eb
Ismael Hamed <1279846+ismaelhamed@users.noreply.github.com>
2021-05-07 18:46:06 +0200

    Upgraded some nuget packages

--- src/Akka.Cluster.Management.Cli/Properties/AssemblyInfo.cs
+++ src/Akka.Cluster.Management.Cli/Properties/AssemblyInfo.cs
@@ -4,11 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Akka.Cluster.Management.Cli")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Akka.Cluster.Management.Cli")]
 [assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,15 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("99a1864a-e1f3-466c-b12c-a30ec554c867")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("0.7.4")]
-[assembly: AssemblyFileVersion("0.7.4")]
